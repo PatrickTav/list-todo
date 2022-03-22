075d7c996549e1072e112bd7bed41a1ab04d6c0a
Patrick Tavares <patricktavares3pc@gmail.com>
2022-03-22 17:02:09 -0300

    master: incluindo aplicação de remover item do todo

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-const formAddTodo = document.querySelector('.form-add-todo')
-const todosContiner = document.querySelector('.todos-container')
-const todoItem = document.createElement('li')
-formAddTodo.addEventListener('submit', event=>{
-    event.preventDefault()
-    const inputValue = event.target.add.value
+const formAddTodo = document.querySelector(".form-add-todo");
+const todosContainer = document.querySelector(".todos-container");
+const todoItem = document.createElement("li");
+
+formAddTodo.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const inputValue = event.target.add.value.trim()
+  console.log(inputValue)
+  if (inputValue.length) {
     todoItem.innerHTML += `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <span>${inputValue}</span>
-        <i class="far fa-trash-alt delete"></i>
-    </li>
-    `
-    todosContiner.insertAdjacentElement('beforeend', todoItem)
-    event.target.reset()
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${inputValue}</span>
+            <i class="far fa-trash-alt delete"></i>
+        </li>
+        `;
+  }
+
+  todosContainer.insertAdjacentElement("beforeend", todoItem);
+  event.target.reset();
+});
+
+todosContainer.addEventListener('click', event=>{
+    const classOfItemList =Array.from(event.target.classList).includes('delete')
+    if(classOfItemList){
+         event.target.parentElement.remove()
+        
+    }
+    
 })
