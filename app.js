4bf77c93e673f2cf0cdb47f721168fddf5312ef8
Patrick Tavares <patricktavares3pc@gmail.com>
2022-03-22 16:49:52 -0300

    master: incluindo aplicação de adicionar todo

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-
+const formAddTodo = document.querySelector('.form-add-todo')
+const todosContiner = document.querySelector('.todos-container')
+const todoItem = document.createElement('li')
+formAddTodo.addEventListener('submit', event=>{
+    event.preventDefault()
+    const inputValue = event.target.add.value
+    todoItem.innerHTML += `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${inputValue}</span>
+        <i class="far fa-trash-alt delete"></i>
+    </li>
+    `
+    todosContiner.insertAdjacentElement('beforeend', todoItem)
+    event.target.reset()
+})
