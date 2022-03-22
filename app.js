436069a2f731681c25d99e46e30b176fbd554e9e
Patrick Tavares <patricktavares3pc@gmail.com>
2022-03-22 19:14:03 -0300

    master: aplicando função de filtros e de mensagem que está vazio

--- app.js
+++ app.js
@@ -1,29 +1,53 @@
 const formAddTodo = document.querySelector(".form-add-todo");
 const todosContainer = document.querySelector(".todos-container");
-const todoItem = document.createElement("li");
+const formSearch = document.querySelector(".form-search");
+const emptyTodo = document.querySelector(".warning");
+
+const WarningMessage = () => {
+  Array.from(todosContainer.children).length
+    ? emptyTodo.classList.add("hidden")
+    : emptyTodo.classList.remove("hidden");
+};
 
 formAddTodo.addEventListener("submit", (event) => {
   event.preventDefault();
-  const inputValue = event.target.add.value.trim()
-  console.log(inputValue)
+  const inputValue = event.target.add.value.trim();
+
   if (inputValue.length) {
-    todoItem.innerHTML += `
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <span>${inputValue}</span>
-            <i class="far fa-trash-alt delete"></i>
-        </li>
-        `;
+    todosContainer.innerHTML += `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${inputValue}</span>
+        <i class="far fa-trash-alt delete"></i>
+    </li>
+    `;
   }
 
-  todosContainer.insertAdjacentElement("beforeend", todoItem);
   event.target.reset();
+  WarningMessage();
 });
 
-todosContainer.addEventListener('click', event=>{
-    const classOfItemList =Array.from(event.target.classList).includes('delete')
-    if(classOfItemList){
-         event.target.parentElement.remove()
-        
-    }
-    
-})
+todosContainer.addEventListener("click", (event) => {
+  const classOfItemList = Array.from(event.target.classList).includes("delete");
+  if (classOfItemList) {
+    event.target.parentElement.remove();
+  }
+  WarningMessage();
+});
+
+formSearch.addEventListener("input", (event) => {
+  const inputSearchValue = event.target.value.trim();
+  Array.from(todosContainer.children)
+    .filter(
+      (todo) => !todo.textContent.toLowerCase().includes(inputSearchValue)
+    )
+    .forEach((todo) => {
+      todo.classList.remove("d-flex");
+      todo.classList.add("hidden");
+    });
+  Array.from(todosContainer.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(inputSearchValue))
+    .forEach((todo) => {
+      todo.classList.remove("hidden");
+      todo.classList.add("d-flex");
+    });
+});
