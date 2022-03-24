709554364dae6e1db501551414b1447fb88d24d3
Patrick Tavares <patricktavares3pc@gmail.com>
2022-03-24 10:14:52 -0300

    master: refatorando o código e manutenção de estilos

--- app.js
+++ app.js
@@ -3,51 +3,89 @@ const todosContainer = document.querySelector(".todos-container");
 const formSearch = document.querySelector(".form-search");
 const emptyTodo = document.querySelector(".warning");
 
+
+
 const WarningMessage = () => {
   Array.from(todosContainer.children).length
     ? emptyTodo.classList.add("hidden")
     : emptyTodo.classList.remove("hidden");
 };
 
-formAddTodo.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const inputValue = event.target.add.value.trim();
 
+
+const addTodo = (inputValue) => {
   if (inputValue.length) {
     todosContainer.innerHTML += `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
+    <li class="list-group-item d-flex justify-content-between align-items-center" data-todo ='${inputValue}'>
         <span>${inputValue}</span>
-        <i class="far fa-trash-alt delete"></i>
+        <i class="far fa-trash-alt" data-trash='${inputValue}'></i>
     </li>
     `;
   }
-
   event.target.reset();
   WarningMessage();
+};
+
+formAddTodo.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const inputValue = event.target.add.value.trim();
+  addTodo(inputValue);
 });
 
-todosContainer.addEventListener("click", (event) => {
-  const classOfItemList = Array.from(event.target.classList).includes("delete");
-  if (classOfItemList) {
-    event.target.parentElement.remove();
+
+const removeTodo = (clickedElement) => {
+  const trashDataValue = clickedElement.dataset.trash;
+  const todo = document.querySelector(
+    `[data-todo="${clickedElement.dataset.trash}"]`
+  );
+  if (trashDataValue) {
+    todo.remove();
+    WarningMessage();
   }
-  WarningMessage();
+};
+
+todosContainer.addEventListener("click", (event) => {
+  const clickedElement = event.target;
+  removeTodo(clickedElement);
 });
+
+
+
+
+const filterTodos = (todos, inputSearchValue, returnMatchedTodos) => {
+  return todos.filter((todo) => {
+    const matchedTodos = todo.textContent
+      .toLowerCase()
+      .includes(inputSearchValue);
+
+    return returnMatchedTodos ? matchedTodos : !matchedTodos;
+  });
+};
+
+const manipulateClasses = (todos, classToAdd, classToRemove) => {
+  todos.forEach((todo) => {
+    todo.classList.remove(classToRemove);
+    todo.classList.add(classToAdd);
+  });
+};
+
+
+
+const hideTodos = (todos, inputSearchValue) => {
+  const todoTohide = filterTodos(todos, inputSearchValue, false);
+  manipulateClasses(todoTohide, "hidden", "d-flex");
+};
+const showTodo = (todos, inputSearchValue) => {
+  const todoToShow = filterTodos(todos, inputSearchValue, true);
+  manipulateClasses(todoToShow, "d-flex", "hidden");
+};
+
+
 
 formSearch.addEventListener("input", (event) => {
   const inputSearchValue = event.target.value.trim();
-  Array.from(todosContainer.children)
-    .filter(
-      (todo) => !todo.textContent.toLowerCase().includes(inputSearchValue)
-    )
-    .forEach((todo) => {
-      todo.classList.remove("d-flex");
-      todo.classList.add("hidden");
-    });
-  Array.from(todosContainer.children)
-    .filter((todo) => todo.textContent.toLowerCase().includes(inputSearchValue))
-    .forEach((todo) => {
-      todo.classList.remove("hidden");
-      todo.classList.add("d-flex");
-    });
+  const todos = Array.from(todosContainer.children);
+
+  hideTodos(todos, inputSearchValue);
+  showTodo(todos, inputSearchValue);
 });
